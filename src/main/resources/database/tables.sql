cdd151dbfcc14bd38ca795e476db220e8e5f1b59
Juan Villadiego <villadiegojm@gmail.com>
2026-01-05 21:30:24 -0500

    T023: Actualizado archivo de scripts de la bd (estructura)

--- src/main/resources/database/tables.sql
+++ src/main/resources/database/tables.sql
@@ -1,35 +1,39 @@
 
 CREATE TABLE IF NOT EXISTS clientes (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    nombre TEXT NOT NULL,
-    cedula INTEGER NOT NULL,
-    telefono INTEGER,
-    estado TEXT NOT NULL
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(150) NOT NULL,
+    cedula INT NOT NULL,
+    telefono VARCHAR(150),
+    estado VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS articulos (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    codigo INTEGER NOT NULL,
-    nombre TEXT NOT NULL,
-    precio REAL NOT NULL,
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    codigo INT NOT NULL,
+    nombre varchar(150) NOT NULL,
+    precio float NOT NULL,
     descripcion TEXT NOT NULL,
-    cantidadstok INTEGER NOT NULL
+    cantidadstok INT NOT NULL
 );
 
 CREATE TABLE facturas (
-	factura_numero INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-	cliente_id INTEGER NOT NULL,
-	total REAL,
-	fecha TEXT DEFAULT (CURRENT_TIMESTAMP),
-	FOREIGN KEY(cliente_id) REFERENCES clientes(cliente_id)
+	factura_numero INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+	cliente_id INT NOT NULL,
+	total float,
+	fecha datetime DEFAULT (CURRENT_TIMESTAMP),
+	FOREIGN KEY(cliente_id) REFERENCES clientes(id)
 );
 
 CREATE TABLE items (
-	item_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-	factura_id INTEGER NOT NULL,
-	articulo_id INTEGER NOT NULL,
-	cantidad INTEGER DEFAULT 1,
-	subtotal REAL NOT NULL,
+	item_id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
+	factura_id INT NOT NULL,
+	articulo_id INT NOT NULL,
+	cantidad INT DEFAULT 1,
+	subtotal float NOT NULL,
 	FOREIGN KEY (factura_id) REFERENCES facturas (factura_numero),
-	FOREIGN KEY (articulo_id) REFERENCES articulos (codigo)
+	FOREIGN KEY (articulo_id) REFERENCES articulos (id)
 );
+
+ALTER TABLE articulos ADD COLUMN porcentaje_descuento INT DEFAULT 0;
+ALTER TABLE items ADD COLUMN precio FLOAT DEFAULT 0.0;
+ALTER TABLE items ADD COLUMN descuento FLOAT DEFAULT 0.0 AFTER subtotal;
